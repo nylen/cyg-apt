c3e7b1593349f6f24d6de5089a6c85cd52b2cbe8
James Nylen <jnylen@gmail.com>
2012-12-04 16:12:43 -0500

    Make install script change to current directory

--- release/install-cyg-apt.sh
+++ release/install-cyg-apt.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/sh
+
+cd "$(dirname "$0")"
+
 # Add Cygwin's public key to the gpg keyring
 gpg --import --no-secmem-warning cygwin.sig
-cp -f cyg-apt /usr/bin/
-cyg-apt setup
+
+# Copy cyg-apt
+cp -vf cyg-apt /usr/local/bin/
+
+# Initialize cyg-apt
+./cyg-apt setup
