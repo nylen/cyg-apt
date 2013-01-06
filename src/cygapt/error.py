27459654077d4ebe0caee332870b8032bafa7491
alquerci <alquerci@email.com>
2013-01-06 22:25:50 +0100

    Fix some typo

--- src/cygapt/error.py
+++ src/cygapt/error.py
@@ -5,7 +5,7 @@
                  <cjcormie@gmail.com> <janneke@gnu.org>
   (c) 2012       James Nylen
                  <jnylen@gmail.com>
-  (c) 2012-2013  Alexandre Querci
+  (c) 2012-2013  Alexandre Quercia
                  <alquerci@email.com>
 
   License: GNU GPL
