ce8d9e214d451f7cb7dbcbbb23db3ecf25e04309
alquerci <alquerci@email.com>
2014-01-01 03:07:09 +0100

    Happy Yew Year

--- src/cygapt/copying.py
+++ src/cygapt/copying.py
@@ -5,7 +5,7 @@
 # Copyright (C) 2002-2009 Jan Nieuwenhuizen  <janneke@gnu.org>
 #               2002-2009 Chris Cormie       <cjcormie@gmail.com>
 #                    2012 James Nylen        <jnylen@gmail.com>
-#               2012-2013 Alexandre Quercia  <alquerci@email.com>
+#               2012-2014 Alexandre Quercia  <alquerci@email.com>
 #
 # For the full copyright and license information, please view the
 # LICENSE file that was distributed with this source code.
@@ -15,7 +15,7 @@ help_message = (
 "Copyright (C) 2002-2009 Jan Nieuwenhuizen  <janneke@gnu.org>{LF}"
 "              2002-2009 Chris Cormie       <cjcormie@gmail.com>{LF}"
 "                   2012 James Nylen        <jnylen@gmail.com>{LF}"
-"              2012-2013 Alexandre Quercia  <alquerci@email.com>{LF}"
+"              2012-2014 Alexandre Quercia  <alquerci@email.com>{LF}"
 "License: GNU GPL version 3 <http://www.gnu.org/licenses/gpl-3.0.html>{LF}"
 "This is free software: you are free to change and redistribute it.{LF}"
 "There is NO WARRANTY, to the extent permitted by law.{LF}"
--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@
 # Copyright (C) 2002-2009 Jan Nieuwenhuizen  <janneke@gnu.org>
 #               2002-2009 Chris Cormie       <cjcormie@gmail.com>
 #                    2012 James Nylen        <jnylen@gmail.com>
-#               2012-2013 Alexandre Quercia  <alquerci@email.com>
+#               2012-2014 Alexandre Quercia  <alquerci@email.com>
 #
 # For the full copyright and license information, please view the
 # LICENSE file that was distributed with this source code.
